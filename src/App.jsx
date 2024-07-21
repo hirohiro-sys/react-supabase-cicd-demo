da6e59459951f36c82509a066aecee5fc47099e4
hirohiro-sys <katohiro8843@icloud.com>
2024-07-21 22:16:59 +0900

    supabaseからデータ取得

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { getAllRecords } from "../utils/supabasefunctions";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("0");
+  const [records, setRecords] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+    const getRecords = async () => {
+      setIsLoading(true);
+      const records = await getAllRecords();
+      setRecords(records);
+      setIsLoading(false);
+    };
+    getRecords();
+  }, []);
+
+  const onClickAddRecord = () => {
+    if ((title === "") | (time === "")) {
+      setError("入力されていない項目があります");
+      return;
+    } else {
+      const newRecords = [...records, { title, time: parseInt(time) || 0 }];
+      setRecords(newRecords);
+      setTitle("");
+      setTime("0");
+      setError("");
+    }
+  };
+  const sumTime = records.reduce((total, rec) => total + rec.time, 0);
   return (
     <>
+      <div className="title">
+        <p>学習内容</p>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div className="time">
+        <p>学習時間</p>
+        <input
+          type="number"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+        />
+        <p>時間</p>
+      </div>
+
+      <p>入力されている学習内容: {title}</p>
+      <p>入力されている学習時間: {time}時間</p>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>学習記録一覧</h1>
+        {isLoading ? (
+          <p>データを取得しています。</p>
+        ) : (
+          <ul>
+            {records.map((record, index) => (
+              <li key={index}>
+                <p>{record.title}</p>: {record.time}時間
+              </li>
+            ))}
+          </ul>
+        )}
+        <button onClick={onClickAddRecord}>登録</button>
+        <p>{error}</p>
+        <p>合計時間: {sumTime}/1000(h)</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ utils/supabasefunctions.js
@@ -0,0 +1,6 @@
+import {supabase}  from "../utils/supabase"
+
+export const getAllRecords = async () => {
+    const records = await supabase.from("study-record").select("*");
+    return records.data;
+}
