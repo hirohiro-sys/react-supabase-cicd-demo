c62035a554e951f39dcf82d2867bb4d3b2b3b53e
hirohiro-sys <katohiro8843@icloud.com>
2024-07-28 07:36:46 +0900

    モックなしで

--- src/tests/componenteSample.spec.jsx
+++ src/tests/componenteSample.spec.jsx
@@ -4,13 +4,43 @@ import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 
 describe("テストの練習", () => {
 
-  test("①タイトルが正常に表示されているか", () => {
+  test("タイトルが正常に表示されているか", () => {
     render(<App />);
     const titleElement = screen.getByTestId("test-title");
     expect(titleElement).toBeInTheDocument();
   });
   
-    test("②要素が追加され1つ増えるか", async () => {
+  test("入力されていない項目がある場合にエラーが表示されるか", async () => {
+    render(<App />);
+    const addButton = screen.getByText("登録");
+    fireEvent.click(addButton);
+    await waitFor(() => {
+      expect(
+        screen.getByText("入力されていない項目があります")
+      ).toBeInTheDocument();
+    });
+  });
+
+
+  test("要素が削除され1つ減るか", async () => {
+    render(<App />);
+
+    await waitFor(() => screen.getAllByTestId("record-list"));
+    let records = screen.getAllByTestId("record-list")[0].querySelectorAll("li");
+    const initialRecordCount = records.length;
+
+    const deleteButtons = screen.getAllByText("削除");
+    fireEvent.click(deleteButtons[0]);
+
+  
+    await waitFor(() => {
+      records = screen.getAllByTestId("record-list")[0].querySelectorAll("li");
+      expect(records.length).toBe(initialRecordCount - 1); 
+    });
+  });
+
+
+  test("要素が追加され1つ増えるか", async () => {
     render(<App />);
     await waitFor(() => screen.getAllByTestId("record-list"));
 
@@ -30,32 +60,5 @@ describe("テストの練習", () => {
     });
   });
 
-    test("③要素が削除され1つ減るか", async () => {
-    render(<App />);
-
-    await waitFor(() => screen.getAllByTestId("record-list"));
-    let records = screen.getAllByTestId("record-list")[0].querySelectorAll("li");
-    const initialRecordCount = records.length;
-
-    const deleteButtons = screen.getAllByText("削除");
-    fireEvent.click(deleteButtons[0]);
-
-  
-    await waitFor(() => {
-      records = screen.getAllByTestId("record-list")[0].querySelectorAll("li");
-      expect(records.length).toBe(initialRecordCount - 1); 
-    });
-  });
-
-  test("④入力されていない項目がある場合にエラーが表示されるか", async () => {
-    render(<App />);
-    const addButton = screen.getByText("登録");
-    fireEvent.click(addButton);
-    await waitFor(() => {
-      expect(
-        screen.getByText("入力されていない項目があります")
-      ).toBeInTheDocument();
-    });
-  });
 
 });
