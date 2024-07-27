2de3710e0457abbba7c8579df3a82aef332a02bd
hirohiro-sys <katohiro8843@icloud.com>
2024-07-27 22:38:13 +0900

    一旦モックなしで

--- src/tests/componenteSample.spec.jsx
+++ src/tests/componenteSample.spec.jsx
@@ -2,21 +2,6 @@ import App from "../App";
 import React from "react";
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 
-const mockGetAllRecords = jest.fn().mockResolvedValue([
-  { id: 1, title: "title1", time: 3 },
-  { id: 2, title: "title2", time: 4 },
-  { id: 3, title: "title3", time: 3 },
-]);
-
-const mockDeleteRecord = jest.fn(() => Promise.resolve());
-
-jest.mock("../../utils/supabasefunctions", () => {
-  return {
-    getAllRecords: () => mockGetAllRecords(),
-    deleteRecord: () => mockDeleteRecord()
-  };
-});
-
 describe("テストの練習", () => {
 
   test("タイトルが正常に表示されているか", () => {
@@ -36,11 +21,44 @@ describe("テストの練習", () => {
     });
   });
 
-  test("レコードが3つ表示されること", async () => {
+  test("要素が削除されること", async () => {
     render(<App />);
+
     await waitFor(() => screen.getAllByTestId("record-list"));
-    const records = screen.getAllByTestId("record-list")[0].querySelectorAll("li");
-    expect(records.length).toBe(3);
+    let records = screen.getAllByTestId("record-list")[0].querySelectorAll("li");
+    const initialRecordCount = records.length;
+
+    const deleteButtons = screen.getAllByText("削除");
+    fireEvent.click(deleteButtons[0]);
+
+  
+    await waitFor(() => {
+      records = screen.getAllByTestId("record-list")[0].querySelectorAll("li");
+      expect(records.length).toBe(initialRecordCount - 1); 
+    });
+  });
+
+  test("要素が追加されること", async () => {
+    render(<App />);
+  
+    await waitFor(() => screen.getAllByTestId("record-list"));
+
+    const titleInput = screen.getByPlaceholderText("学習内容を入力");
+    const timeInput = screen.getByPlaceholderText("時間を入力");
+    const addButton = screen.getByText("登録");
+  
+    // レコード追加前の要素数を取得
+    const recordsBefore = screen.getAllByTestId("record-list")[0].querySelectorAll("li").length;
+  
+    fireEvent.change(titleInput, { target: { value: "新しい学習" } });
+    fireEvent.change(timeInput, { target: { value: "120" } });
+    fireEvent.click(addButton);
+  
+    // レコード追加後の要素数を取得し、増加を確認
+    await waitFor(() => {
+      const recordsAfter = screen.getAllByTestId("record-list")[0].querySelectorAll("li").length;
+      expect(recordsAfter).toBe(recordsBefore + 1);
+    });
   });
 
 });
